f13ffab8f96b3b49b6dfae9ab716c98ec6357e7a
erik rahman <118335576+erikrahman2@users.noreply.github.com>
2025-01-10 19:48:35 +0700

    Add files via upload

---
+++ js-dasar/0coba.js
@@ -0,0 +1,35 @@
+// console.log(1 + 20 + 2 - 5);
+// alert("hello world");
+
+// variabel biasanya menggunakan var, let, const
+// let, const lebih modern
+var namo = "kerl",
+  usio = 23,
+  hobi = "tido";
+console.log(namo);
+let nama = "john doe";
+const umur = 30;
+console.log(nama + umur);
+
+// string bisa menggunakan "" atau ''
+let call = 'brandon said"no"'.length;
+let talk = "lisa' the \n girl";
+console.log(call);
+console.log(talk);
+
+var x = 10;
+console.log(x > 3);
+
+// popup box
+
+// alert("hello world!");
+
+// var namee = prompt("masukkan nama: ");
+// alert(namee);
+
+var tes = confirm("are ya sure?");
+if (tes === true) {
+  alert("y re mine");
+} else {
+  alert("gone");
+}
