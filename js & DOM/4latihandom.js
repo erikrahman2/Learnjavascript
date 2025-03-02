224f165374cf61dfe66260884a7aa66e0e6c0a5e
erik rahman <118335576+erikrahman2@users.noreply.github.com>
2025-03-02 15:02:28 +0700

    Add files via upload

--- js & DOM/4latihandom.js
+++ js & DOM/4latihandom.js
@@ -1,10 +1,11 @@
-// simply change color button
+// 1. simply change color button
 let btnclr = document.getElementById(tchangecolor);
 tchangecolor.onclick = function () {
   //   document.body.style.backgroundColor = "red";
   document.body.classList.toggle("red");
 };
 
+// 2. change random color
 let newbtn = document.createElement("button");
 let newtextbtn = document.createTextNode("change random color");
 newbtn.appendChild(newtextbtn);
@@ -15,5 +16,31 @@ newbtn.addEventListener("click", function () {
   let g = Math.round(Math.random() * 255 + 1);
   let b = Math.round(Math.random() * 255 + 1);
 
+  document.body.style.backgroundColor = "rgb(" + r + "," + g + "," + b + ")";
+});
+
+// 3. slider color
+let smerah = document.querySelector("input[name=smerah]");
+let shijau = document.querySelector("input[name=shijau]");
+let sbiru = document.querySelector("input[name=sbiru]");
+
+smerah.addEventListener("input", function () {
+  let r = smerah.value;
+  let g = shijau.value;
+  let b = sbiru.value;
+  document.body.style.backgroundColor = "rgb(" + r + "," + g + "," + b + ")";
+});
+
+shijau.addEventListener("input", function () {
+  let r = smerah.value;
+  let g = shijau.value;
+  let b = sbiru.value;
+  document.body.style.backgroundColor = "rgb(" + r + "," + g + "," + b + ")";
+});
+
+sbiru.addEventListener("input", function () {
+  let r = smerah.value;
+  let g = shijau.value;
+  let b = sbiru.value;
   document.body.style.backgroundColor = "rgb(" + r + "," + g + "," + b + ")";
 });
